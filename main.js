02835d7809cdc70d190fb0c70ad849ad5d3e051d
Ayan Roy <ayanr8614@gmail.com>
2025-02-18 23:39:33 +0530

    added gitIgnore blank file, and modified main.js

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-//loading the express lib.
 const express = require("express");
-
-//loading cors lib.
 const cors = require("cors");
-const foodRouter = require("./routes/foods.routes");
-const userRouter = require("./routes/user.route");
-
-//env lib.
 const env = require("dotenv").config();
 
 //define port and host globally
 var port = process.env.HOST_PORT || 3000;
 var host = process.env.HOST || "127.0.0.1";
 
-//create an instance of express
 const app = express();
 app.use(cors());
+
 //creating server static resourse
 app.use(express.static("public"));
 console.log(`SSR is working on public folder`);
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+const userRouter = require("./routes/user.route");
 app.use("/api/users",userRouter)
+
+const foodRouter = require("./routes/foods.routes");
 app.use('/api/foods',foodRouter);
 
 
